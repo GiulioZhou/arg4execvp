656025505fb927900e3a5231d639269850dadf97
Giulio <giuliozhou@gmail.com>
2016-05-09 20:25:27 +0200

    I don't like it

--- arg4execvp.c
+++ arg4execvp.c
@@ -7,6 +7,76 @@
 #define MAX_LINE 1024
 
 
+
+
+void split(char *command[], char line[]){
+	
+	char *token;
+	int spaces = 0;
+ 
+	strtok(line, "\n");
+	token = strtok(line, " ");
+	
+	while( token != NULL )
+	{
+		spaces++;
+		command[spaces-1] = token;
+		token = strtok(NULL, " ");
+	}
+	
+	command[spaces] = NULL;
+	
+}
+
+int main(int argc, char *argv[]){
+	
+	if (argc != 2) {
+		fprintf(stderr, "Error: Expected 1 parameter.\n"
+				"Usage: %s <pathname>\n", argv[0]);
+		return(EXIT_FAILURE);
+	}
+	
+	char *path = argv[1]; //indirizzo del file da leggere
+	char *cmd[MAX_ARGS]; //vettore dei parametri
+	char cmdString[MAX_LINE]; //riga di codice
+	char *s =cmdString;
+	FILE *f;
+	int pid;
+	
+	f = fopen(path, "r");
+	if (f == NULL){
+		printf("cannot open the file\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	while ( fgets (cmdString, MAX_LINE, f)!=NULL){
+		
+		if ( (pid = fork()) == -1) {
+			return(EXIT_FAILURE);
+		}
+		
+		if (pid == 0) {	//child
+	
+			split (cmd, cmdString);
+			execvp(cmd[0], cmd);
+			
+			perror(cmd[0]);
+			return(EXIT_FAILURE);
+			
+		} else {
+			wait(NULL);	//wait for child to terminate
+		}
+	}
+	fclose(f);
+	return 0;
+}
+
+
+
+//---- PROBABLY MORE EFFICIENT BUT NOT WORKING ----//
+
+//@The idea is to use dinamic sized array for the command line and arg
+
 /* char **split (char **argv2, char *stringa){
 	
 	char *token;
@@ -20,32 +90,32 @@
 	
 	while( token != NULL )
 	{
-		nSpazi++;
-		
-		//realloc doesn't work -> invalid old size 
-		// and probably other segmentation fault
-		argv2 = realloc(argv2, sizeof(char*)* nSpazi);
-		
-		if( argv2 == NULL)
-			exit(EXIT_FAILURE);
-		argv2[nSpazi-1] = token;
-		
-		token = strtok(NULL, s);
+		 nSpazi++;
+		 
+		 //realloc doesn't work -> invalid old size
+		 // and probably other segmentation fault
+		 argv2 = realloc(argv2, sizeof(char*)* nSpazi);
+		 
+		 if( argv2 == NULL)
+		 exit(EXIT_FAILURE);
+		 argv2[nSpazi-1] = token;
+		 
+		 token = strtok(NULL, s);
 	}
 	
 	argv2 = realloc(argv2, sizeof(char*) * (nSpazi + 1));
 	argv2[nSpazi] = NULL;
 	return argv2;
-}
+ }
  
-
-int main(int argc, char *argv[]){
+ 
+ int main(int argc, char *argv[]){
 	
 	
 	if (argc != 2) {
-		fprintf(stderr, "Error: Expected 1 parameter.\n"
-				"Usage: %s <pathname>\n", argv[0]);
-		return(EXIT_FAILURE);
+ fprintf(stderr, "Error: Expected 1 parameter.\n"
+ "Usage: %s <pathname>\n", argv[0]);
+ return(EXIT_FAILURE);
 	}
  
 	char *path = argv[1];
@@ -61,27 +131,27 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	
 	while (( read = getline( &cmdString, &n, f)) != -1){
-		
-		
-		if ( (pid = fork()) == -1) {
+ 
+ 
+		 if ( (pid = fork()) == -1) {
 			perror("fork");
 			return(EXIT_FAILURE);
-		}
-		
-		if (pid == 0) {	//child
-			
+		 }
+		 
+		 if (pid == 0) {	//child
+		 
 			cmd = split (cmd, cmdString);
 			execvp(cmd[0], cmd);
-			
+ 
 			perror(cmd[0]);
 			return(EXIT_FAILURE);
-			
-		} else {
-
+		 
+		 } else {
+		 
 			wait(NULL);	//wait for child to terminate
-		}
-		
-		cmd = NULL;
+		 }
+		 
+		 cmd = NULL;
 	}
 	
 	
@@ -90,85 +160,6 @@ int main(int argc, char *argv[]){
 	
 	
 	return 0;
-}
+ }
  
  */
-
-void split(char *command[], char line[]){
-	
-	char *token;
-	int spaces = 0;
- 
-	strtok(line, "\n");
-	
-	token = strtok(line, " ");
-	
-	while( token != NULL )
-	{
-		spaces++;
-		
-		command[spaces-1] = token;
-		
-		token = strtok(NULL, " ");
-	}
-	
-	command[spaces] = NULL;
-	
-}
-
-int main(int argc, char *argv[]){
-	
-	
-	if (argc != 2) {
-		fprintf(stderr, "Error: Expected 1 parameter.\n"
-				"Usage: %s <pathname>\n", argv[0]);
-		return(EXIT_FAILURE);
-	}
-
-	
-	char *path = argv[1]; //indirizzo del file da leggere
-	char *cmd[MAX_ARGS]; //vettore dei parametri
-	char cmdString[MAX_LINE]; //riga di codice
-	char *s =cmdString;
-	FILE *f;
-	int pid;
-	
-	
-	f = fopen(path, "r");
-	
-	
-	if (f == NULL){
-		printf("cannot open the file\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	
-	while ( fgets (cmdString, MAX_LINE, f)!=NULL){
-		
-		if ( (pid = fork()) == -1) {
-			return(EXIT_FAILURE);
-		}
-		
-		if (pid == 0) {	//child
-			
-			split (cmd, cmdString);
-			execvp(cmd[0], cmd);
-			
-			perror(cmd[0]);
-			return(EXIT_FAILURE);
-			
-		} else {
-			
-			wait(NULL);	//wait for child to terminate
-		}
-		
-	}
-	
-	fclose(f);
-	
-	return 0;
-}
-
-
-
-
