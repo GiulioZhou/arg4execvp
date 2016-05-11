6c0971485a478a6ba1bb7dcc7e4cd99ec21b7982
Giulio <giuliozhou@gmail.com>
2016-05-12 00:40:15 +0200

    don't like it

--- arg4execvp.c
+++ arg4execvp.c
@@ -2,12 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #define MAX_ARGS 64
 #define MAX_LINE 1024
 
-
-
+//split the command line into { command, par1, par2, ..., parn, NULL}
 
 void split(char *command[], char line[]){
 	
@@ -43,11 +41,15 @@ int main(int argc, char *argv[]){
 	FILE *f;
 	int pid;
 	
+	//open the file
+	
 	f = fopen(path, "r");
 	if (f == NULL){
 		printf("cannot open the file\n");
 		exit(EXIT_FAILURE);
 	}
+	
+	//Gets all the line and execute
 	
 	while ( fgets (cmdString, MAX_LINE, f)!=NULL){
 		
@@ -73,93 +75,3 @@ int main(int argc, char *argv[]){
 
 
 
-//---- PROBABLY MORE EFFICIENT BUT NOT WORKING ----//
-
-//@The idea is to use dinamic sized array for the command line and arg
-
-/* char **split (char **argv2, char *stringa){
-	
-	char *token;
-	int nSpazi = 0;
- 
-	//strtok doesn't work -> bus error:10
- 
-	strtok(stringa, "\n");
-	
-	token = strtok(str, " ");
-	
-	while( token != NULL )
-	{
-		 nSpazi++;
-		 
-		 //realloc doesn't work -> invalid old size
-		 // and probably other segmentation fault
-		 argv2 = realloc(argv2, sizeof(char*)* nSpazi);
-		 
-		 if( argv2 == NULL)
-		 exit(EXIT_FAILURE);
-		 argv2[nSpazi-1] = token;
-		 
-		 token = strtok(NULL, s);
-	}
-	
-	argv2 = realloc(argv2, sizeof(char*) * (nSpazi + 1));
-	argv2[nSpazi] = NULL;
-	return argv2;
- }
- 
- 
- int main(int argc, char *argv[]){
-	
-	
-	if (argc != 2) {
- fprintf(stderr, "Error: Expected 1 parameter.\n"
- "Usage: %s <pathname>\n", argv[0]);
- return(EXIT_FAILURE);
-	}
- 
-	char *path = argv[1];
-	char *cmdString = NULL;
-	char **cmd;
-	size_t n;
-	ssize_t read;
-	FILE *f;
-	int pid;
-	
-	f = fopen("path", "r");
-	if (f == NULL)
-		exit(EXIT_FAILURE);
-	
-	while (( read = getline( &cmdString, &n, f)) != -1){
- 
- 
-		 if ( (pid = fork()) == -1) {
-			perror("fork");
-			return(EXIT_FAILURE);
-		 }
-		 
-		 if (pid == 0) {	//child
-		 
-			cmd = split (cmd, cmdString);
-			execvp(cmd[0], cmd);
- 
-			perror(cmd[0]);
-			return(EXIT_FAILURE);
-		 
-		 } else {
-		 
-			wait(NULL);	//wait for child to terminate
-		 }
-		 
-		 cmd = NULL;
-	}
-	
-	
-	fclose(f);
-	
-	
-	
-	return 0;
- }
- 
- */
